2e96eec0c7cbf146cdab409f9a10d92865bb2c58
Bjørnar Luteberget <bjlut@railcomplete.no>
2018-06-12 15:15:29 +0200

    wip

---
+++ test_rc.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+from rc import *
+
+class Tests(unittest.TestCase):
+    def test_hello(self):
+        self.assertEqual(Model, None)
+
+if __name__ == '__main__':
+    unittest.main()
